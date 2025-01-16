304eccc56eeb9f70c953ae9b73ef2a0313330b3f
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-01-16 19:54:17 +0100

    Interactie gemaakt
    #7 Helaas niet aan de darkmode toegekomen op een kleine body verandering na dan. Wel scroll animations gemaakt!

--- script/script.js
+++ script/script.js
@@ -1,5 +1,31 @@
-const clicking = document.querySelector('.breadandbutter')
-
-    clicking.addEventListener('click', function(){
-        turn.classList.add('turning');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+    } 
+    else {
+        entry.target.classList.remove('show');
+    }
     });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const pagebody = document.getElementsByTagName('body')[0];
+const togglebtn = document.getElementById('theme');
+const pageSection = document.getElementsByTagName('section')[0];
+
+togglebtn.addEventListener('click', (e) => {
+    pagebody.classList.toggle('lightmode');
+    pageSection.classList.toggle('lightmode');
+})
+
+
+// turning bread and butter
+
+
+// scroll CSS animation
+
