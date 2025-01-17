71ef2e5781dba95523f64587b0d1943cd668f4c6
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-01-17 09:50:44 +0100

    #3

--- script/script.js
+++ script/script.js
@@ -17,10 +17,11 @@ hiddenElements.forEach((el) => observer.observe(el));
 const pagebody = document.getElementsByTagName('body')[0];
 const togglebtn = document.getElementById('theme');
 const pageSection = document.getElementsByTagName('section')[0];
+const textelement = document.querySelectorAll('.lightmode');
+const headlogo = document.querySelector('.logo-darkmode');
 
 togglebtn.addEventListener('click', (e) => {
     pagebody.classList.toggle('lightmode');
-    pageSection.classList.toggle('lightmode');
 })
 
 
