219a2a085c1ba5e3f10c9c68ddda86a88edfb3f3
miel775 <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-01-23 11:23:44 +0100

    laatste aanpassingen
    aanpassingen onder anderen voor kleurblindheid voor de lighthouse test.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting) {
         entry.target.classList.add('show');
     } 
@@ -15,6 +14,7 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 const togglebtn = document.getElementById('theme');
 const togglebtnmbl = document.querySelector('.togglebttn');
+
 
 togglebtn.addEventListener('click', () => {
     const allElements = document.querySelectorAll('*');
@@ -46,6 +46,62 @@ togglebtnmbl.addEventListener('click', () => {
     }
 });
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const csszonesbttn = document.getElementById('zones');
+    const grayscalebttn = document.getElementById('grayscale'); 
+    const bodyElement = document.body; 
+
+    csszonesbttn.addEventListener('click', () => {
+        bodyElement.classList.toggle('zones');
+    });
+
+    grayscalebttn.addEventListener('click', () => {
+        bodyElement.classList.toggle('grayscale');
+    });
+});
+
+    
+
+
+const emailInput = document.getElementById("email");
+const invalidMsg = document.getElementById("invalid");
+const successMsg = document.getElementById("success");
+const form = document.querySelector(".email");
+
+function validateEmail(emailValue) {
+    let value = /\S+@\S+\.\S+/;
+    return value.test(emailValue);
+}
+
+emailInput.addEventListener("invalid", function (e) {
+    let inputValue = e.target;
+    console.log(inputValue.validity);
+    if (inputValue.validity.typeMismatch || inputValue.validity.valueMissing) {
+      inputValue.setCustomValidity(" ");
+      invalidMsg.classList.add("active");
+      successMsg.classList.remove("active");
+      this.classList.add("invalid");
+    } else {
+      invalidMsg.classList.remove("active");
+      successMsg.classList.add("active");
+      this.classList.remove("invalid");
+    }
+  });
+  emailInput.addEventListener("input", function () {
+    invalidMsg.classList.remove("active");
+    this.classList.remove("invalid");
+  });
+
+
+
+emailInput.addEventListener("invalid", function (e) {
+    let inputValue = e.target;
+    if (inputValue.validity.typeMismatch || inputValue.validity.valueMissing) {
+        inputValue.setCustomValidity(" ");
+        invalidMsg.classList.add("invalid")
+    }
+} )
 
 
 
